7dd243e893c5089fd73335d6486d40cc1b247264
Eric Hinderager <eric@topshelfpros.com>
2025-11-16 23:33:29 -0700

    Change mobile theme color from yellow to blue (#06649b)

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
-  themeColor: "#FFC845",
+  themeColor: "#06649b",
 };
 
 export default function RootLayout({
